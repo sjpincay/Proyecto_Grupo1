0a951ad4223486b7a7bf49ce1ad07d291513c701
DanielReborn01 <wamaya@espol.edu.ec>
2023-06-26 20:59:45 -0500

    Create Utilidades.java

---
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Utilidades.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Es una clase que implementa métodos estáticos de utilidades.
+ * @author danie
+ */
+public class Utilidades {
+    public static ArrayList<String> LeeFichero(String nombrearchivo) {
+        ArrayList<String> lineas = new ArrayList<>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(nombrearchivo);
+            fr = new FileReader(archivo,StandardCharsets.UTF_8);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                lineas.add(linea);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return lineas;
+
+    }
+    /**
+     * Es un método que pregunta al usuario y retorna la respuesta en tipo String
+     * @param mensaje
+     * @return 
+     */
+    public static String solicitarEntrada(String mensaje){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(mensaje);
+        String dato= sc.nextLine();
+        return dato;
+    }
+}
+
