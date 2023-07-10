98c9d0959dea06a81c25cf34d6316dd786a013dc
Sheyla Pincay <137127024+sjpincay@users.noreply.github.com>
2023-07-09 22:55:26 -0500

    Update Cliente.java
    agregando documentación

--- Proyecto1/src/main/java/com/mycompany/proyecto1/Cliente.java
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Cliente.java
@@ -4,40 +4,45 @@
  */
 package com.mycompany.proyecto1;
 
-//import Enums.TipoPerfil;
-//import static ManejoArchivos.ManejoArchivos.LeerValidando;
-//import java.util.ArrayList;
-import static ManejoArchivos.ManejoArchivos.LeerValidando;
 import Enums.TipoPerfil;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
+
 /**
  * Clase Cliente que representa a un cliente en el sistema.
+ * 
+ * Esta clase hereda de la clase Usuario y agrega funcionalidades específicas para un cliente,
+ * como el número de tarjeta de crédito, los puntos de licencia, la lista de vehículos y la
+ * capacidad de consultar multas y agendar revisiones técnicas.
+ * 
+ * El código está documentado internamente con comentarios para una mejor comprensión.
+ * 
  */
 public class Cliente extends Usuario {
-    private String numTarjetaCredito;
-    private int puntosLicencia;
-    private final ArrayList<Vehiculo> listVehiculos = initVehiculos();
-    private final ArrayList<Date> horarios = Sistema.horarios; // Corregido el nombre del tipo de lista
-    private final ArrayList<String[]> datosClientes = LeerValidando("clientes.txt", true);
+    private String numTarjetaCredito; // Número de tarjeta de crédito del cliente
+    private int puntosLicencia; // Puntos de licencia del cliente
+    private final ArrayList<Vehiculo> listVehiculos = initVehiculos(); // Lista de vehículos del cliente
+    private final ArrayList<Date> horarios = Sistema.horarios; // Lista de horarios disponibles para revisiones técnicas
+    private final ArrayList<String[]> datosClientes = LeerValidando("clientes.txt", true); // Datos de los clientes almacenados en un archivo
     private final Scanner sc = new Scanner(System.in);
 
     /**
      * Constructor de la clase Cliente.
      *
-     * @param cedula      Cédula del cliente
-     * @param nombres     Nombres del cliente
-     * @param edad        Edad del cliente
-     * @param correo      Correo electrónico del cliente
-     * @param usuario     Usuario del cliente
-     * @param Password    Contraseña del cliente
-     * @param tipoPerfil  Tipo de perfil del cliente
+     * @param cedula     Cédula del cliente
+     * @param nombres    Nombres del cliente
+     * @param edad       Edad del cliente
+     * @param correo     Correo electrónico del cliente
+     * @param usuario    Usuario del cliente
+     * @param Password   Contraseña del cliente
+     * @param tipoPerfil Tipo de perfil del cliente
      */
-    public Cliente(String cedula, String nombres, int edad, String correo, String usuario, String Password, TipoPerfil tipoPerfil) {
+    public Cliente(String cedula, String nombres, int edad, String correo, String usuario, String Password,
+            TipoPerfil tipoPerfil) {
         super(cedula, nombres, edad, correo, usuario, Password, tipoPerfil);
-        
+
         // Recuperar datos del cliente desde un archivo (clientes.txt) y asignarlos a las variables correspondientes
         for (String[] dato : datosClientes) {
             if (dato[0].equals(cedula)) {
@@ -53,8 +58,9 @@ public class Cliente extends Usuario {
      * @return ArrayList de vehículos pertenecientes al cliente.
      */
     private ArrayList<Vehiculo> initVehiculos() {
-        ArrayList<Vehiculo> vehiculosTo = Sistema.listaVehiculoss;
+        ArrayList<Vehiculo> vehiculosTo = Sistema.listaVehiculoss; // Obtener la lista de vehículos del sistema
         ArrayList<Vehiculo> vehiculoReturn = new ArrayList<>();
+
         for (Vehiculo vehiculo : vehiculosTo) {
             if (vehiculo.getOwner().equals(super.cedula)) {
                 vehiculoReturn.add(vehiculo);
@@ -70,36 +76,35 @@ public class Cliente extends Usuario {
     public void consultarMultas() {
         int opcion = 0;
         Scanner entrada = new Scanner(System.in);
-        
+
         do {
-            System.out.println("""
-                    Desea ver las multas con su número de cédula (1) +
-                    o desea ver con su placa (2): """);
+            System.out.println("Desea ver las multas con su número de cédula (1) ");
+            System.out.println("o desea ver con su placa (2): ");
             opcion = entrada.nextInt();
             entrada.nextLine();
         } while (opcion > 2 || opcion < 1);
-        
+
         switch (opcion) {
             case 1 -> {
                 System.out.println("Ingrese su cédula: ");
                 String cedulaInput = entrada.nextLine();
-                
+
                 if (!cedulaInput.equals(this.cedula)) {
                     System.out.println("Sus datos no coinciden, intente nuevamente");
                     return;
                 }
-                
+
                 for (Vehiculo vehiculo : listVehiculos) {
                     // Si se selecciona la opción de cédula, se muestra las multas de todos los vehículos del cliente
                     vehiculo.mostrarMultas();
                 }
                 break;
             }
-                
+
             case 2 -> {
                 System.out.println("Ingrese su placa: ");
                 String placa = entrada.nextLine();
-                 
+
                 for (Vehiculo vehiculo : listVehiculos) {
                     // Si se selecciona la opción de placa, se muestra las multas del vehículo con la placa ingresada
                     if (vehiculo.getPlaca().equals(placa)) {
@@ -118,52 +123,52 @@ public class Cliente extends Usuario {
         System.out.println("Ingrese su placa: ");
         String placa = sc.nextLine();
         Vehiculo vehiculoRevision = null;
-        
+
         for (Vehiculo vehiculo : listVehiculos) {
             if (vehiculo.getPlaca().equals(placa)) {
                 vehiculoRevision = vehiculo;
                 break;
             }
         }
-        
+
         if (vehiculoRevision == null) {
             System.out.println("No se encontró su vehículo en la base de datos");
             return;
         }
-        
+
         if (!vehiculoRevision.getMultas().isEmpty()) {
             // Entra si tiene multas
             System.out.println("Lo siento, usted tiene multas pendientes");
             vehiculoRevision.mostrarMultas();
             return;
         }
-        
+
         System.out.println("Ud. no tiene multas, por favor elija un horario");
         int contador = 1;
         int opcionHorario = 0;
-        
+
         for (Date horario : horarios) {
-            System.out.println(contador + ". " + horario);
+            System.out.println(contador + ". "+ horario);
             contador++;
         }
-        
+
         do {
             System.out.println("Elija el horario para la revisión: ");
             opcionHorario = sc.nextInt();
             sc.nextLine(); // Limpiando el buffer
         } while (opcionHorario > horarios.size() || opcionHorario <= 0);
-        
+
         // Obtenemos la fecha seleccionada
         Date fecha = horarios.get(opcionHorario - 1);
 
         System.out.println(this + "Se ha registrado su cita para "
-                + new SimpleDateFormat("dd-MM-yyyy").format(fecha) +
-                " a las " + new SimpleDateFormat("HH::ss").format(fecha));
-        
+                + new SimpleDateFormat("dd-MM-yyyy").format(fecha) + " a las "
+                + new SimpleDateFormat("HH::ss").format(fecha));
+
         System.out.println("Valor a pagar: " + valorPagar(placa));
-        
+
         Revision revision = new Revision(super.cedula, placa, fecha);
-        
+
         revision.addRevision(); // Añadir revisión a la base de datos
         horarios.remove(opcionHorario - 1); // Se quita un horario disponible
         Sistema.removeHorarario(opcionHorario - 1);
@@ -180,7 +185,7 @@ public class Cliente extends Usuario {
     public double valorPagar(String placa) {
         double base = 150.0;
         int puntosPerdidos = 0;
-        
+
         // Obtener la cantidad de puntos perdidos
         for (Vehiculo vehiculo : listVehiculos) {
             if (vehiculo.getPlaca().equals(placa)) {
@@ -188,10 +193,12 @@ public class Cliente extends Usuario {
                 break;
             }
         }
-        
+
         return base + (puntosPerdidos * 10);
     }
-    
+
+    // Getters y Setters
+
     public String getNumTarjetaCredito() {
         return numTarjetaCredito;
     }
