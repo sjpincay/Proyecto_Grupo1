9eae14fcb507cc4ac3d612999e349ff1cbb05520
Sheyla Pincay <137127024+sjpincay@users.noreply.github.com>
2023-07-09 22:35:39 -0500

    Update Pago.java

--- Proyecto1/src/main/java/com/mycompany/proyecto1/Pago.java
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Pago.java
@@ -9,24 +9,43 @@ import java.util.Date;
 
 /**
  *
- * Clase transaccional que permitira gestionar los pagos que se hagan, esto sera 
+ * Clase transaccional que permitirá gestionar los pagos que se hagan, esto será 
  * mediante un operador, el cliente directamente no puede hacerlo
+ * 
+ * Esta clase representa un pago realizado en el sistema. Tiene atributos que
+ * almacenan información relacionada con el pago, como el cliente, la revisión
+ * asociada, el valor a pagar, el modo de pago, etc.
+ * 
+ * El código está documentado internamente con comentarios para una mejor comprensión.
+ * 
+ * 
  * @author wal26
  */
 public class Pago {
-    private int codigoPago;
-    private Cliente cliente;
-    private Revision revision;
-    private double valorPagar;
-    private char modoPagar;
-    private double valorPagarFinal;
-    private Date fechaPago;
-    private String razonPago;
+    private int codigoPago; // Identificador único para cada pago
+    private Cliente cliente; // Objeto Cliente asociado con el pago
+    private Revision revision; // Objeto Revision asociado con el pago
+    private double valorPagar; // Valor a pagar inicialmente
+    private char modoPagar; // Modo de pago (carácter)
+    private double valorPagarFinal; // Valor a pagar final después de aplicar descuentos o cargos adicionales
+    private Date fechaPago; // Fecha en la que se realizó el pago
+    private String razonPago; // Descripción o razón para el pago
     
-    private static int codigo = 0;
+    private static int codigo = 0; // Variable estática para generar códigos de pago secuenciales
 
+    /**
+     * Constructor para un pago asociado con una revisión.
+     * 
+     * @param cliente El objeto Cliente asociado con el pago
+     * @param revision El objeto Revision asociado con el pago
+     * @param valorPagar El valor a pagar inicialmente
+     * @param modoPagar El modo de pago (carácter)
+     * @param valorPagarFinal El valor a pagar final después de aplicar descuentos o cargos adicionales
+     * @param fechaPago La fecha en la que se realizó el pago
+     * @param razonPago La descripción o razón para el pago
+     */
     public Pago(Cliente cliente, Revision revision, double valorPagar, char modoPagar, double valorPagarFinal, Date fechaPago, String razonPago) {
-        this.codigoPago = codigo++;
+        this.codigoPago = codigo++; // Asignar un nuevo código de pago secuencial
         this.cliente = cliente;
         this.revision = revision;
         this.valorPagar = valorPagar;
@@ -36,21 +55,18 @@ public class Pago {
         this.razonPago = razonPago;
     }
 
-    
     /**
-     * Constructor que no requiere reservacion, ya que unicamente sera para
-     * pago de multa
+     * Constructor para un pago de multa sin una revisión asociada.
      * 
-     * 
-     * @param cliente
-     * @param valorPagar
-     * @param modoPagar
-     * @param valorPagarFinal
-     * @param fechaPago
-     * @param razonPago 
+     * @param cliente El objeto Cliente asociado con el pago
+     * @param valorPagar El valor a pagar inicialmente
+     * @param modoPagar El modo de pago (carácter)
+     * @param valorPagarFinal El valor a pagar final después de aplicar descuentos o cargos adicionales
+     * @param fechaPago La fecha en la que se realizó el pago
+     * @param razonPago La descripción o razón para el pago
      */
     public Pago(Cliente cliente, double valorPagar, char modoPagar, double valorPagarFinal, Date fechaPago, String razonPago) {
-        this.codigoPago = codigo++;
+        this.codigoPago = codigo++; // Asignar un nuevo código de pago secuencial
         this.cliente = cliente;
         this.valorPagar = valorPagar;
         this.modoPagar = modoPagar;
@@ -59,6 +75,11 @@ public class Pago {
         this.razonPago = razonPago;
     }
 
+    /**
+     * Devuelve una representación en forma de cadena del objeto Pago.
+     * 
+     * @return Una cadena que representa el objeto Pago
+     */
     @Override
     public String toString() {
         return codigoPago + "," + cliente.cedula + "," + valorPagar + ","
@@ -67,16 +88,30 @@ public class Pago {
                 razonPago;
     }
 
+    /**
+     * Establece el modo de pago.
+     * 
+     * @param modoPagar El modo de pago a establecer
+     */
     public void setModoPagar(char modoPagar) {
         this.modoPagar = modoPagar;
     }
 
+    /**
+     * Establece el valor a pagar final.
+     * 
+     * @param valorPagarFinal El valor a pagar final a establecer
+     */
     public void setValorPagarFinal(double valorPagarFinal) {
         this.valorPagarFinal = valorPagarFinal;
     }
     
+    /**
+     * Agrega el pago al sistema.
+     * 
+     * Llama al método estático createPago() de la clase Sistema para agregar el pago al sistema.
+     */
     public void addPago(){
         Sistema.createPago(this);
     }
-    
 }
