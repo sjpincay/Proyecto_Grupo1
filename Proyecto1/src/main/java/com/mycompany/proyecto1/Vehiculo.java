30aeae4c2a5d22e60a9eb9c1f798fb8e3c68480d
Sheyla Pincay <137127024+sjpincay@users.noreply.github.com>
2023-07-09 22:42:48 -0500

    Update Vehiculo.java
    agregando la documentación

--- Proyecto1/src/main/java/com/mycompany/proyecto1/Vehiculo.java
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Vehiculo.java
@@ -8,15 +8,36 @@ import java.util.ArrayList;
  *
  * @author sjpin
  */
+/**
+ * Clase que representa un Vehículo en el sistema.
+ * 
+ * Esta clase define los atributos y métodos relacionados con un vehículo, como el propietario,
+ * la placa, la marca, el modelo, el año, el chasis, el color y las multas asociadas.
+ * 
+ * El código está documentado internamente con comentarios para una mejor comprensión.
+ * 
+ */
 public class Vehiculo {
-    private String owner;
-    private String placa;
-    private String marca;
-    private String modelo;
-    private String year;
-    private String chasis;
-    private String color;
-    private ArrayList<Multa> multas;  
+    private String owner; // Propietario del vehículo
+    private String placa; // Placa del vehículo
+    private String marca; // Marca del vehículo
+    private String modelo; // Modelo del vehículo
+    private String year; // Año del vehículo
+    private String chasis; // Chasis del vehículo
+    private String color; // Color del vehículo
+    private ArrayList<Multa> multas; // Lista de multas asociadas al vehículo
+
+    /**
+     * Constructor para la clase Vehiculo.
+     * 
+     * @param owner El propietario del vehículo
+     * @param placa La placa del vehículo
+     * @param marca La marca del vehículo
+     * @param modelo El modelo del vehículo
+     * @param year El año del vehículo
+     * @param chasis El chasis del vehículo
+     * @param color El color del vehículo
+     */
     public Vehiculo(String owner, String placa, String marca, String modelo, String year, String chasis, String color) {
         this.owner = owner;
         this.placa = placa;
@@ -26,12 +47,16 @@ public class Vehiculo {
         this.chasis = chasis;
         this.color = color;
 
-        multas = initMultas();
+        multas = initMultas(); // Inicializar la lista de multas asociadas al vehículo
     }
     
-    
+    /**
+     * Método que inicializa la lista de multas del vehículo.
+     * 
+     * @return La lista de multas asociadas al vehículo
+     */
     public ArrayList<Multa> initMultas(){
-        ArrayList<Multa> mult = Sistema.listaMultlas;
+        ArrayList<Multa> mult = Sistema.listaMultlas; // Obtener la lista de multas del sistema
         ArrayList<Multa> multaReturn = new ArrayList<>();
         
         // Filtrar las multas por placa
@@ -42,12 +67,13 @@ public class Vehiculo {
         }
         return multaReturn;
     }
-     /**
+
+    /**
      * Método estático que obtiene un objeto Vehiculo según su placa.
-     *
-     * @param vehiculos Lista de vehículos
-     * @param placa Placa del vehículo a buscar
-     * @return Objeto Vehiculo correspondiente a la placa o null si no se encuentra
+     * 
+     * @param vehiculos La lista de vehículos
+     * @param placa La placa del vehículo a buscar
+     * @return El objeto Vehiculo correspondiente a la placa o null si no se encuentra
      */
     public static Vehiculo getVehiculo(ArrayList<Vehiculo> vehiculos, String placa){
         for(Vehiculo vehi: vehiculos){
@@ -58,14 +84,13 @@ public class Vehiculo {
         
         return null;
     }
+    
     /**
      * Método que muestra las multas del vehículo y retorna el valor total a pagar.
-     *
-     * @return Valor total a pagar por las multas del vehículo
+     * 
+     * @return El valor total a pagar por las multas del vehículo
      */
     public double mostrarMultas() {
-        
-
         System.out.println("------------------------------------------------");
         System.out.println("               DETALLE DE MULTAS                ");
         System.out.println("------------------------------------------------");
@@ -75,23 +100,21 @@ public class Vehiculo {
             System.out.println(multa.getCedula() + " | " + multa.getPlaca() + " | " + multa.getInfraccion() + " | " +
                     multa.getValor() + " | " + multa.getFechaInfraccion() + " | " + multa.getFechaNotificacion() + " | " +
                     multa.getPuntos());
-
         }
 
         double valorTotal = getValor();
         if (valorTotal > 0) {
-            
             System.out.println("\n\nTOTAL A PAGAR: " + valorTotal);
             System.out.println("\nPara pagar puede acercarse a la agencia más cercana.");
         }
         return valorTotal;
     }
+    
     /**
      * Método que calcula el valor total a pagar por las multas del vehículo.
-     *
-     * @return Valor total a pagar por las multas del vehículo
+     * 
+     * @return El valor total a pagar por las multas del vehículo
      */
-    
     public double getValor(){
         double valorTotal = 0.0;
         for (Multa multa : multas) {
@@ -99,12 +122,12 @@ public class Vehiculo {
         }
         return valorTotal;
     }
+    
     /**
      * Método que calcula la suma de los puntos perdidos por las multas del vehículo.
-     *
-     * @return Suma de puntos perdidos por las multas del vehículo
+     * 
+     * @return La suma de los puntos perdidos por las multas del vehículo
      */
-    
     public int totalPuntosPerdidos() {
         int puntosTotales = 0;
 
@@ -114,8 +137,9 @@ public class Vehiculo {
 
         return puntosTotales;
     }
+
     // Getters y Setters
-    
+
     public String getOwner() {
         return owner;
     }
@@ -179,6 +203,4 @@ public class Vehiculo {
     public void setMultas(ArrayList<Multa> multas) {
         this.multas = multas;
     }
-    
 }
-
